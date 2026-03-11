d568472d3cf403ee93eb9fca1da6cf970643921d
Mohamed Abo Hebil <mabuhebil@gmail.com>
2026-03-11 05:32:48 +0200

    update

---
+++ src/app/_componates/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="bg-black text-white">
+      <h2>footer</h2>
+      <h2>footer</h2>
+      <h2>footer</h2>
+      <h2>footer</h2>
+    </footer>
+  );
+}
