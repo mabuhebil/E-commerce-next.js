89fc7ea980573345aa5820588b4358a8164e9429
Mohamed Abo Hebil <mabuhebil@gmail.com>
2026-03-26 09:36:51 +0200

    update

--- src/next_auth/nextAuth.config.ts
+++ src/next_auth/nextAuth.config.ts
@@ -1,6 +1,8 @@
 import { NextAuthOptions } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 
+import { jwtDecode } from "jwt-decode";
+
 export const nextAuthConfig: NextAuthOptions = {
   providers: [
     Credentials({
@@ -33,10 +35,14 @@ export const nextAuthConfig: NextAuthOptions = {
         if (res.ok) {
           const { name, email } = finalRes.user;
 
+          // that is userObj  will  not be show in tkn  thats is real data
+
+          const data: { id: string } = jwtDecode(finalRes.token);
           return {
             name,
             email,
-            id: "",
+            id: data.id,
+            tokenCredentials: finalRes.token,
           };
         }
 
@@ -44,10 +50,30 @@ export const nextAuthConfig: NextAuthOptions = {
       },
     }),
   ],
+
+  callbacks: {
+    jwt: function (param) {
+      // param.token that is defualt valus will be shown in tkn
+
+      if (param.user) {
+        param.token.routeToken = param.user.tokenCredentials;
+        param.token.id = param.user.id;
+      }
+
+      console.log("jwt param", param);
+      return param.token;
+    },
+    session: function (param) {
+      param.session.user.id = param.token.id;
+      console.log("param from session", param);
+      return param.session;
+    },
+  },
+
   jwt: {
     maxAge: 60 * 60 * 24 * 3,
   },
-  pages:{
-    signIn:'/login'
-  }
+  pages: {
+    signIn: "/login",
+  },
 };
